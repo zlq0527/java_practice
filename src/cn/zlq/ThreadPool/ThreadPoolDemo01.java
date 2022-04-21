24db1280e9ec71adb591b1b3cf0c61c763cf7c73
zlq <1142401783@qq.com>
2022-04-21 21:14:59 +0800

    ""

--- src/cn/zlq/ThreadPool/ThreadPoolDemo01.java
+++ src/cn/zlq/ThreadPool/ThreadPoolDemo01.java
@@ -1,6 +1,8 @@
 package cn.zlq.ThreadPool;
 
+import java.util.Stack;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @ Author     ：赵棱泉.
@@ -8,24 +10,25 @@ import java.util.concurrent.*;
  * @ Description：
  */
 public class ThreadPoolDemo01 {
-	private static final Executor executors = Executors.newFixedThreadPool(5);
-	static ExecutorService executorService = Executors.newFixedThreadPool(5);
-	public static void main(String[] args) throws ExecutionException, InterruptedException {
-		FutureTask<Integer> futureTask = new FutureTask<Integer>(new Test());
-		executorService.submit(new Test());
-//		new Thread(futureTask).start();
-		System.out.println(futureTask.get());
-	}
-}
-class Test implements Callable {
-	@Override
-	public Integer call() throws Exception {
-		int a=0;
-		try {
-			a = 1 / 0;
-		} catch (Exception e){
-			throw new RuntimeException("0不能做被除数");
+	static AtomicInteger integer = new AtomicInteger();
+	public static void main(String[] args) throws InterruptedException {
+		Thread thread = new Thread(() -> {
+//			synchronized (integer) {
+				for (int i = 0; i < 100; i++) {
+					System.out.println(Thread.currentThread().getName() + ">>>>>>" + integer);
+					integer.incrementAndGet();
+				}
+//			}
+		});
+		ThreadPoolExecutor executor = new ThreadPoolExecutor(
+				2,
+				4,
+				5,
+				TimeUnit.SECONDS,
+				new LinkedBlockingQueue<>());
+		for (int i = 0; i < 5; i++) {
+			executor.execute(thread);
 		}
-		return 0;
+		executor.shutdown();
 	}
 }
