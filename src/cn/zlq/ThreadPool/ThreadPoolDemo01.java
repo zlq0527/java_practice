5b129dbeb7b44bcba377b3ff72aa1a21b3ecffa7
zlq <1142401783@qq.com>
2022-04-22 16:50:42 +0800

    ""

--- src/cn/zlq/ThreadPool/ThreadPoolDemo01.java
+++ src/cn/zlq/ThreadPool/ThreadPoolDemo01.java
@@ -1,6 +1,5 @@
 package cn.zlq.ThreadPool;
 
-import java.util.Stack;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -13,19 +12,17 @@ public class ThreadPoolDemo01 {
 	static AtomicInteger integer = new AtomicInteger();
 	public static void main(String[] args) throws InterruptedException {
 		Thread thread = new Thread(() -> {
-//			synchronized (integer) {
 				for (int i = 0; i < 100; i++) {
 					System.out.println(Thread.currentThread().getName() + ">>>>>>" + integer);
-					integer.incrementAndGet();
+					integer.getAndIncrement();
 				}
-//			}
 		});
 		ThreadPoolExecutor executor = new ThreadPoolExecutor(
 				2,
 				4,
 				5,
 				TimeUnit.SECONDS,
-				new LinkedBlockingQueue<>());
+				new LinkedBlockingQueue<>(1));
 		for (int i = 0; i < 5; i++) {
 			executor.execute(thread);
 		}
